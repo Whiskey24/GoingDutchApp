093ecc16dd118a94f559094310b1610e029e3b5e
Bert Santema <atsantema@hotmail.com>
2015-06-17 00:22:24 +0200

    Working on disabling navigation and keeping the sidemenu available.

--- www/js/app.js
+++ www/js/app.js
@@ -7,154 +7,177 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('GoingDutchApp', ['ionic', 'starter.controllers', 'starter.services'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleLightContent();
-    }
-  });
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
-
-  // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: "/tab",
-    abstract: true,
-    templateUrl: "templates/tabs.html"
-  })
-  
-  // setup an abstract state for the groups directive
-    .state('group', {
-    url: "/group",
-    abstract: true,
-    templateUrl: "templates/group.html"
-  })  
-
-    /*
-    .state('groupmenu', {
-      url: "/groupmenu",
-      abstract: true,
-      templateUrl: "templates/group-menu.html"
-    })
-    
-    
-    .state('groupmenu.settings', {
-      url: "/settings",
-      views: {
-        'menuContent' :{
-          templateUrl: "templates/menu-settings.html"
-        }
-      }
-    })
-    */
-  
-  // Each tab has its own nav history stack:
-
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
-    }
-  })
-
-  .state('tab.groups', {
-      url: '/groups',
-      views: {
-        'tab-groups': {
-          templateUrl: 'templates/tab-groups.html',
-          controller: 'GroupsCtrl'
-        }
-      }
-    })  
-  
-  
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
-  })
-
-  .state('group.members', {
-      url: '/members',
-      views: {
-        'group-members': {
-          templateUrl: 'templates/group-members.html',
-          controller: 'MembersCtrl'
-        }
-      }
+    .run(function ($ionicPlatform) {
+        $ionicPlatform.ready(function () {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            }
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleLightContent();
+            }
+        });
     })
 
-  .state('group.expenses', {
-      url: '/expenses',
-      views: {
-        'group-expenses': {
-          templateUrl: 'templates/group-expenses.html',
-          controller: 'ExpensesCtrl'
-        }
-      }
-    })    
-    
-  .state('group.events', {
-      url: '/events',
-      views: {
-        'group-events': {
-          templateUrl: 'templates/group-events.html',
-          controller: 'EventsCtrl'
-        }
-      }
-    })
-    
-    .state('group.settings', {
-      url: "/settings",
-      views: {
-        'menuContent' :{
-          templateUrl: "templates/group-settings.html"
-        }
-      }
-    }); 
-    
-   /*
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
-        }
-      }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })
-    */
-  
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+    .config(function ($stateProvider, $urlRouterProvider) {
 
-});
+        // Ionic uses AngularUI Router which uses the concept of states
+        // Learn more here: https://github.com/angular-ui/ui-router
+        // Set up the various states which the app can be in.
+        // Each state's controller can be found in controllers.js
+        $stateProvider
+
+            // setup an abstract state for the tabs directive
+            .state('tab', {
+                url: "/tab",
+                abstract: true,
+                templateUrl: "templates/tabs.html"
+            })
+
+            // setup an abstract state for the groups directive
+            .state('group', {
+                url: "/group",
+                abstract: true,
+                templateUrl: "templates/group.html"
+            })
+
+            /*
+             .state('groupmenu', {
+             url: "/groupmenu",
+             abstract: true,
+             templateUrl: "templates/group-menu.html"
+             })
+
+
+             .state('groupmenu.settings', {
+             url: "/settings",
+             views: {
+             'menuContent' :{
+             templateUrl: "templates/menu-settings.html"
+             }
+             }
+             })
+             */
+
+            // Each tab has its own nav history stack:
+
+            .state('tab.dash', {
+                url: '/dash',
+                views: {
+                    'tab-dash': {
+                        templateUrl: 'templates/tab-dash.html',
+                        controller: 'DashCtrl'
+                    }
+                }
+            })
+
+            .state('tab.groups', {
+                url: '/groups',
+                views: {
+                    'tab-groups': {
+                        templateUrl: 'templates/tab-groups.html',
+                        controller: 'GroupsCtrl'
+                    }
+                }
+            })
+
+
+            .state('tab.account', {
+                url: '/account',
+                views: {
+                    'tab-account': {
+                        templateUrl: 'templates/tab-account.html',
+                        controller: 'AccountCtrl'
+                    }
+                }
+            })
+
+            .state('group.members', {
+                url: '/members',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/group-members.html',
+                        controller: 'MembersCtrl'
+                    }
+                }
+            })
+
+            .state('group.expenses', {
+                url: '/expenses',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/group-expenses.html',
+                        controller: 'ExpensesCtrl'
+                    }
+                }
+            })
+
+            .state('group.events', {
+                url: '/events',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/group-events.html',
+                        controller: 'EventsCtrl'
+                    }
+                }
+            })
+
+            .state('group.settings', {
+                url: "/settings",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/group-settings.html",
+                        controller: 'SettingsCtrl'
+                    }
+                }
+            })
+
+            .state('group.settle', {
+                url: "/settle",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/group-settle.html"
+                    }
+                }
+            })
+
+            .state('group.achievements', {
+                url: "/achievements",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/group-achievements.html"
+                    }
+                }
+            })
+
+
+        ;
+
+
+        /*
+         .state('tab.chats', {
+         url: '/chats',
+         views: {
+         'tab-chats': {
+         templateUrl: 'templates/tab-chats.html',
+         controller: 'ChatsCtrl'
+         }
+         }
+         })
+         .state('tab.chat-detail', {
+         url: '/chats/:chatId',
+         views: {
+         'tab-chats': {
+         templateUrl: 'templates/chat-detail.html',
+         controller: 'ChatDetailCtrl'
+         }
+         }
+         })
+         */
+
+        // if none of the above states are matched, use this as the fallback
+        $urlRouterProvider.otherwise('/tab/dash');
+
+    });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -13,6 +13,9 @@ angular.module('starter.controllers', [])
     .controller('EventsCtrl', function ($scope) {
     })
 
+    .controller('SettingsCtrl', function ($scope) {
+    })
+
     .controller('ChatsCtrl', function ($scope, Chats) {
         // With the new view caching in Ionic, Controllers are only called
         // when they are recreated or on app start, instead of every page change.
