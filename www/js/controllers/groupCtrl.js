07846a6029b36c746711367e130280efdb75f2f9
Bert Santema <atsantema@hotmail.com>
2015-07-05 23:25:11 +0200

    Got first unit test to work.  Need to refactor controllers and service in separate modules.

--- www/js/controllers/groupCtrl.js
+++ www/js/controllers/groupCtrl.js
@@ -2,10 +2,32 @@
  * Created by Whiskey on 19-6-2015.
  */
 
+/*
+    angular.module('GoingDutchApp.controllers', []).controller('GroupCtrl', ['$scope', 'gdApi', function    ($scope, gdApi) {
+
+        $scope.getGroupCurrency = function(gid) {
+            return gdApi.getGroupCurrency(gid);
+        };
+
+        $scope.groups = gdApi.getGroups();
+
+        $scope.test = true;
+
+        $scope.settings = {
+            enableFriends: true
+        };
+    }]);
+
+*/
+//angular.module('myApp.controllers').controller('Ctrlr1', ['$scope', '$http', function($scope, $http){
+//
+//}]);
+
+
 (function () {
     'use strict';
 
-    angular.module('GoingDutchApp').controller('GroupCtrl', ['$scope', 'gdApi', GroupCtrl]);
+    angular.module('GoingDutchApp.controllers').controller('GroupCtrl', ['$scope', 'gdApi', GroupCtrl]);
 
     function GroupCtrl($scope, gdApi) {
 
@@ -14,6 +36,12 @@
         };
 
         $scope.groups = gdApi.getGroups();
+
+        $scope.test = true;
+
+        $scope.settings = {
+            enableFriends: true
+        };
     }
 
 })();
