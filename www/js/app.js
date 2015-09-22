d38141ee39d909c362e516bbb1877ee43a691425
Bert Santema <atsantema@hotmail.com>
2015-09-23 00:04:57 +0200

    Start test with online api

--- www/js/app.js
+++ www/js/app.js
@@ -2,9 +2,11 @@ angular.module('GoingDutchApp', ['ionic', 'GoingDutchApp.controllers', 'isoCurre
 
     .constant('gdConfig', (function () {
         var host = 'http://api.gdutch.dev';
+        //var host = 'https://api.santema.eu/GoingDutchApi';
         return {
             host: host,
             port: 80,
+            //port: 443,
             url_groups: host + '/groups',
             url_users: host + '/users',
             url_expenses: host + '/group/{gid}/expenses',
